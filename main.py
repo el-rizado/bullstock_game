385bb869963e54c653fe31f8ad3b175c375678c3
el-rizado <amirhosseinmohseni1996@yahoo.com>
2020-11-24 07:42:07 +0330

    write first function for generate random number

---
+++ main.py
@@ -0,0 +1,24 @@
+from random import randint, choice
+
+
+def generate_random():
+    flag = False
+    out_number = 0
+    while not flag:
+        number = randint(1000, 9999)
+        count_num = 0
+        count = {}
+        for digit in str(number):
+            if digit in count:
+                count[digit] += 1
+            else:
+                count[digit] = 1
+        for key in count:
+            if count[key] == 1:
+                count_num += 1
+        if count_num == 4:
+            out_number = number
+            flag = True
+    return out_number
+
+
